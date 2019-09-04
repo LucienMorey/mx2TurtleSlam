4d5d385f1d1469202cb936dca92331f07e8eeb21
jmcnamara256 <jmcnamara256@gmail.com>
2019-09-04 11:53:39 +1000

    Added turtlebot3 simulation package

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/jesse/Documents/mx2TurtleSlam/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/jesse/Documents/mx2TurtleSlam/devel;/opt/ros/kinetic".split(';') if "/home/jesse/Documents/mx2TurtleSlam/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/jesse/Documents/mx2TurtleSlam/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/jesse/Documents/mx2TurtleSlam/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/jesse/Documents/mx2TurtleSlam/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/jesse/Documents/mx2TurtleSlam/build"
+export PYTHONPATH="/home/jesse/Documents/mx2TurtleSlam/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jesse/Documents/mx2TurtleSlam/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/jesse/Documents/mx2TurtleSlam/src:$ROS_PACKAGE_PATH"
