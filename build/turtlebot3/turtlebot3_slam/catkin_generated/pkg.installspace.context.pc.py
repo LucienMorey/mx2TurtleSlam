51df5e076d8b4704c3cbf9a97ac1a71232f95fd8
Lucien Morey <lucienp@live.com.au>
2019-09-15 03:15:02 +1000

    removing shit again

--- build/turtlebot3/turtlebot3_slam/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/jesse/Documents/mx2TurtleSlam/install/include".split(';') if "/home/jesse/Documents/mx2TurtleSlam/install/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;sensor_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "turtlebot3_slam"
-PROJECT_SPACE_DIR = "/home/jesse/Documents/mx2TurtleSlam/install"
-PROJECT_VERSION = "1.2.1"
