31d68c2797f68edbb8086a0e8a6bf6dcd4cf4d01
Jesse <jmcnamara256@gmail.com>
2019-10-07 02:01:30 +1100

    workin path planning prototype

---
+++ src/astar_path_planner/nodes/astar_path_planning.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python
+# Path planning node returns a list of poses forming a path
+# Subscibes to OccupancyGrid
+# Run map_server with target map to publish OccupancyGrid messages
+# Publishes ????
+
+import rospy
+from nav_msgs.msg import OccupancyGrid
+import matplotlib.pyplot as plt
+import math
+
+#import to stop matplot warnings in terminal
+import warnings
+import matplotlib.cbook
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
+
+# Global variables
+map_x           = 0
+map_y           = 0
+map_res         = 0         # cm/node
+ox, oy          = [], []    # obstacle maps
+show_animation  = True
+
+
+class astar_planner:
+
+    def __init__(self, ox, oy, reso, rr):
+        self.reso = reso
+        self.rr = rr
+        self.calc_obstacle_map(ox, oy)
+        self.motion = self.get_motion_model()
+
+    class Node:
+        def __init__(self, x, y, cost, pind):
+            self.x = x  # index of grid
+            self.y = y  # index of grid
+            self.cost = cost
+            self.pind = pind
+
+        def __str__(self):
+            return str(self.x) + "," + str(self.y) + "," + str(self.cost) + "," + str(self.pind)
+
+    def planning(self, sx, sy, gx, gy):
+        print("Planning started")
+        """
+        A star path search
+        input:
+            sx: start x position [m]
+            sy: start y position [m]
+            gx: goal x position [m]
+            gx: goal x position [m]
+        output:
+            rx: x position list of the final path
+            ry: y position list of the final path
+        """
+
+        nstart = self.Node(self.calc_xyindex(sx, self.minx),
+                           self.calc_xyindex(sy, self.miny), 0.0, -1)
+        ngoal = self.Node(self.calc_xyindex(gx, self.minx),
+                          self.calc_xyindex(gy, self.miny), 0.0, -1)
+
+        open_set, closed_set = dict(), dict()
+        open_set[self.calc_grid_index(nstart)] = nstart
+
+        while 1:
+            if len(open_set) == 0:
+                print("Open set is empty...")
+                break
+
+            c_id = min(open_set, key=lambda o: open_set[o].cost + self.calc_heuristic(ngoal, open_set[o]))
+            current = open_set[c_id]
+
+            # show graph
+            if show_animation:  # pragma: no cover
+                plt.plot(self.calc_grid_position(current.x, self.minx),
+                         self.calc_grid_position(current.y, self.miny), "xc")
+                if len(closed_set.keys()) % 10 == 0:
+                    plt.pause(0.001)
+
+            if current.x == ngoal.x and current.y == ngoal.y:
+                print("Goal Found!")
+                ngoal.pind = current.pind
+                ngoal.cost = current.cost
+                break
+
+            # Remove the item from the open set
+            del open_set[c_id]
+
+            # Add it to the closed set
+            closed_set[c_id] = current
+
+            # expand_grid search grid based on motion model
+            for i, _ in enumerate(self.motion):
+                node = self.Node(current.x + self.motion[i][0],
+                                 current.y + self.motion[i][1],
+                                 current.cost + self.motion[i][2], c_id)
+                n_id = self.calc_grid_index(node)
+
+
+                # If the node is not safe, do nothing
+                if not self.verify_node(node):
+                    continue
+
+                if n_id in closed_set:
+                    continue
+
+                if n_id not in open_set:
+                    open_set[n_id] = node  # discovered a new node
+                else:
+                    if open_set[n_id].cost > node.cost:
+                        # This path is the best until now. record it
+                        open_set[n_id] = node
+
+        rx, ry = self.calc_final_path(ngoal, closed_set)
+
+        return rx, ry
+
+    def calc_final_path(self, ngoal, closedset):
+        # generate final course
+        rx, ry = [self.calc_grid_position(ngoal.x, self.minx)], [
+            self.calc_grid_position(ngoal.y, self.miny)]
+        pind = ngoal.pind
+        while pind != -1:
+            n = closedset[pind]
+            rx.append(self.calc_grid_position(n.x, self.minx))
+            ry.append(self.calc_grid_position(n.y, self.miny))
+            pind = n.pind
+
+
+        return rx, ry
+
+    @staticmethod
+    def calc_heuristic(n1, n2):
+        w = 1.0  # weight of heuristic
+        d = w * math.sqrt((n1.x - n2.x) ** 2 + (n1.y - n2.y) ** 2)
+        return d
+
+    def calc_grid_position(self, index, minp):
+        """
+        calc grid position
+        :param index:
+        :param minp:
+        :return:
+        """
+        pos = index * self.reso + minp
+        return pos
+
+    def calc_xyindex(self, position, min_pos):
+        return int(round((position - min_pos) / self.reso))
+
+    def calc_grid_index(self, node):
+        return (node.y - self.miny) * self.xwidth + (node.x - self.minx)
+
+    def verify_node(self, node):
+        px = self.calc_grid_position(node.x, self.minx)
+        py = self.calc_grid_position(node.y, self.miny)
+
+        if px < self.minx:
+            return False
+        elif py < self.miny:
+            return False
+        elif px >= self.maxx:
+            return False
+        elif py >= self.maxy:
+            return False
+
+        # collision check
+        if self.obmap[node.x][node.y]:
+            return False
+
+        return True
+
+    def calc_obstacle_map(self, ox, oy):
+
+        self.minx = int(round(min(ox)))
+        self.miny = int(round(min(oy)))
+        self.maxx = int(round(max(ox)))
+        self.maxy = int(round(max(oy)))
+
+        self.xwidth = int(round((self.maxx - self.minx) / self.reso))
+        self.ywidth = int(round((self.maxy - self.miny) / self.reso))
+
+        # obstacle map generation
+        self.obmap = [[False for i in range(self.ywidth)]for i in range(self.xwidth)]
+        for ix in range(self.xwidth):
+            x = self.calc_grid_position(ix, self.minx)
+            for iy in range(int(self.ywidth)):
+                y = self.calc_grid_position(iy, self.miny)
+                for iox, ioy in zip(ox, oy):
+                    d = math.sqrt((iox - x) ** 2 + (ioy - y) ** 2)
+                    if d <= self.rr:
+                        self.obmap[ix][iy] = True
+                        break
+
+    @staticmethod
+    def get_motion_model():
+        # dx, dy, cost
+        motion = [[1, 0, 1],
+                  [0, 1, 1],
+                  [-1, 0, 1],
+                  [0, -1, 1],
+                  [-1, -1, math.sqrt(2)],
+                  [-1, 1, math.sqrt(2)],
+                  [1, -1, math.sqrt(2)],
+                  [1, 1, math.sqrt(2)]]
+
+        return motion
+
+'''
+####################################
+###########End of Class#############
+####################################
+'''
+
+def print_map(msg):
+	f = open('map.txt', 'a+')
+	f.truncate(0)
+
+	print("Found map!")
+
+	for y in range(msg.info.height -1, 0, -1):
+		for x in range(msg.info.width):
+			i = msg.info.width*y + x
+			if(x < msg.info.width - 1):
+				c = to_trinary(msg.data[i])
+				f.write(c)
+			else:
+				f.write("\r\n")
+
+	f.close()
+	rospy.signal_shutdown("quit")
+
+
+
+def to_trinary(p):
+	if (p == 100):
+		return str(1)
+	elif (p == 0):
+		return str(0)
+	else:
+		return str(2)
+
+def map_callback(msg):
+    map = msg
+    count = 0
+
+    global ox, oy, map_res
+    map_res = 1#map.info.resolution
+
+    for y in range(map.info.height):
+        for x in range(map.info.width):
+            i = y * map.info.width + x
+            if (to_trinary(map.data[i]) == '1'):
+                ox.append(x)
+                oy.append(y)
+                count += 1
+    
+    print("Map found!\nHeight: {}\nWidth: {}\nResolution: {}" .format(map.info.height, map.info.width, map.info.resolution))
+    print("Found {} obstacle points" .format(count))
+
+if __name__=="__main__":
+    rospy.init_node('map_writer')
+    map_sub = rospy.Subscriber('map', OccupancyGrid, map_callback)
+    #map_pub = rospy.Publisher("normalised_map", OccupancyGrid, queue_size = 10)
+
+    sx = 10
+    sy = 5
+    gx = 13
+    gy = 57
+    robot_radius = 0.2  # m
+
+    while 1:
+        if(ox):
+            if show_animation:  # pragma: no cover
+                plt.plot(ox, oy, ".k")
+                plt.plot(sx, sy, "og")
+                plt.plot(gx, gy, "xb")
+                plt.grid(True)
+                plt.axis("equal")
+    
+            astar = astar_planner(ox, oy, map_res, robot_radius)
+            rx, ry = astar.planning(sx, sy, gx, gy)
+
+
+            if show_animation:  # pragma: no cover
+                plt.plot(rx, ry, "-r")
+                plt.show()
+                break;
+
+
--- src/astar_path_planner/nodes/map_writer.py
+++ src/astar_path_planner/nodes/map_writer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # subscribe to occupancy grid msg
 # occupancy grid published by map_server map_server
 # output occupancy grid as a text file
@@ -5,11 +6,13 @@
 import rospy
 from nav_msgs.msg import OccupancyGrid
 
-def map_callback(msg):
+def print_map(msg):
 	f = open('map.txt', 'a+')
 	f.truncate(0)
 
-	for y in range(msg.info.height -1,0, -1):
+	print("Found map!")
+
+	for y in range(msg.info.height -1, 0, -1):
 		for x in range(msg.info.width):
 			i = msg.info.width*y + x
 			if(x < msg.info.width - 1):
@@ -17,8 +20,22 @@ def map_callback(msg):
 				f.write(c)
 			else:
 				f.write("\r\n")
+	
 	f.close()
-	rospy.signal_shutdown("msg recieved")
+	rospy.signal_shutdown("quit")
+
+def map_callback(msg):
+	map = msg
+	print("map %s"  %type(map))
+	print("map.data %s" %type(map.data))
+	for i in range(len(map.data)):
+		a = map.data[i]
+		#print(a)
+		#map.data[i] = to_trinary(a)
+	print_map(map)
+
+	rospy.signal_shutdown("quit")
+
 
 
 def to_trinary(p):
@@ -30,8 +47,8 @@ def to_trinary(p):
 		return str(2)
 
 if __name__=="__main__":
-
     rospy.init_node('map_writer')
     map_sub = rospy.Subscriber('map', OccupancyGrid, map_callback)
+    map_pub = rospy.Publisher("normalised_map", OccupancyGrid, queue_size = 10)
 
     rospy.spin()
